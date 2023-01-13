9217b371a094d1fdc213f17b94142d26477dfe58
leonardo <leonardo.nunes@cybergenios.com.br>
2023-01-13 15:32:12 -0300

    firs test

--- packages/model/src/Model.tsx
+++ packages/model/src/Model.tsx
@@ -2,5 +2,5 @@ import * as React from "react"
 import { ModelDiv } from "./Model.styles"
 
 export const Model = () => {
-  return <ModelDiv>Model Test</ModelDiv>
+  return <ModelDiv>Model Test2</ModelDiv>
 }
